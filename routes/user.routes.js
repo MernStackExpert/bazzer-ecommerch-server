6fb8483f9ee08c95ecf946d37544ea237de9b89f
MernStackExpert <mdnirob30k@gmail.com>
2026-02-25 12:15:19 +0600

    [ADDED]: Add protected routes for profile update and password change

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { registerUser, verifyOTP } = require('../controllers/users.controller');
+const { verifyToken } = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/verify-otp', verifyOTP);
 router.post('/login', loginUser);
 router.post('/verify-login-otp', verifyLoginOTP);
+
+router.put('/update-profile', verifyToken, updateProfile);
+router.put('/update-password', verifyToken, updatePassword);
 
 module.exports = router;
