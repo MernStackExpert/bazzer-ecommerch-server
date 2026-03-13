e9c65d96420cc28f22baa9fc3441f69ba78d733a
MernStackExpert <mdnirob30k@gmail.com>
2026-03-13 11:14:47 +0600

    [UPDATED]: Update seller routes for stats, orders, and product listing APIs

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getSellerStats, getSellerOrders } = require("../controllers/seller.controller");
+const { getSellerStats, getSellerOrders, getSellerProductsList } = require("../controllers/seller.controller");
 const router = express.Router();
 
 router.get("/stats", getSellerStats);
 
 router.get("/orders", getSellerOrders);
+
+router.get("/products",  getSellerProductsList);
 
 module.exports = router;
